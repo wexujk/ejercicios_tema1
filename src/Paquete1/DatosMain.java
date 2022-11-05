3d5d7e5d639bcd3b8c7155c08b125005c43194ee
wexujk <carlospervas@gmail.com>
2022-11-04 21:06:41 -0600

    Initial commit

---
+++ src/Paquete1/DatosMain.java
@@ -0,0 +1,38 @@
+package Paquete1;
+
+public class DatosMain {
+    public static void main(String[] args) {
+
+        //  Numericos
+
+        byte variable1 = 7;
+        short variable2 =11;
+        int variable3 = 5;
+        long variable4 = 4;
+
+        // Decimales
+        float variable5 = 3.14F;
+        double variable6 = 4.14d;
+        // booleano
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        // Texto
+        String variable9 = "Hello";
+        char variable10 = 's';
+        System.out.println("Tipos de datos ");
+
+        System.out.println("byte: " + variable1);
+        System.out.println("short: " + variable2);
+        System.out.println("int: " + variable3);
+        System.out.println("long: " + variable4);
+        System.out.println("float: " + variable5);
+        System.out.println("double: " + variable6);
+
+        System.out.println("boolean: " + variable7);
+        System.out.println("boolean: " + variable8);
+        System.out.println("char: " + variable10);
+        System.out.println("String: "+ variable9);
+
+    }
+}
